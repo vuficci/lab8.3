37a3bad8b3e373f56a697cf48e9ba48490b5e384
vuficci <158140230+vuficci@users.noreply.github.com>
2024-06-21 02:04:04 +0300

    Add files via upload

---
+++ 8.py
@@ -0,0 +1,48 @@
+DATABASE = {
+    'Серёга': 'Омск',
+    'Соня': 'Москва',
+    'Миша': 'Москва',
+    'Дима': 'Челябинск',
+    'Алина': 'Красноярск',
+    'Егор': 'Пермь',
+    'Коля': 'Красноярск'
+}
+
+# Новая функция, она возвращает правильное словосочетание, склоняя слово "друзья" 
+# в зависимости от того, какое число передано в аргументе friends_count
+def format_friends_count(friends_count):
+    if friends_count == 1:
+        return '1 друг'
+    elif 2 <= friends_count <= 4:
+        return f'{friends_count} друга'
+    else:
+        return f'{friends_count} друзей'
+
+def process_anfisa(query):
+    if query == 'сколько у меня друзей?':
+        count = len(DATABASE)
+        friends_count_str = format_friends_count(count)
+        return f'У тебя {friends_count_str}.'
+    elif query == 'кто все мои друзья?':
+        friends_string = ', '.join(DATABASE)
+        return f'Твои друзья: {friends_string}'
+    elif query == 'где все мои друзья?':
+        unique_cities = set(DATABASE.values())
+        cities_string = ', '.join(unique_cities)
+        return f'Твои друзья в городах: {cities_string}'
+    else:
+        return '<неизвестный запрос>'
+
+def process_query(query):
+    name, question = query.split(', ', 1)
+    if name == 'Анфиса':
+        return process_anfisa(question)
+    else:
+        return None
+
+print('Привет, я Анфиса!')
+print(process_query('Анфиса, сколько у меня друзей?'))
+print(process_query('Анфиса, кто все мои друзья?'))
+print(process_query('Анфиса, где все мои друзья?'))
+print(process_query('Анфиса, кто виноват?'))
+print(process_query('Соня, ты где?'))
